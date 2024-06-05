d4eb95a08f48224e5532065d68d69ed584727ce4
Jesús Ramírez <125490310+Subbar26@users.noreply.github.com>
2024-06-05 11:59:49 -0500

    resubiendo el back v1

---
+++ food-for-life-backend/food-for-life-backend/src/test/java/com/example/food_for_life/food_for_life_backend/FoodForLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_for_life.food_for_life_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodForLifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
