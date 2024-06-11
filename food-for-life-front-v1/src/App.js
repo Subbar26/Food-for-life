4037e897ea781eb9335e3b3f709ec7711f49d230
Subbar26 <jesusdavi1408@gmail.com>
2024-06-11 09:57:11 -0500

    route recipe

--- food-for-life-front-v1/src/App.js
+++ food-for-life-front-v1/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header'; // Importar el componente Header
 import Login from './components/Login';
 import PaginaPrincipal from './components/PaginaPrincipal';
+import RecipeCard from './components/Recipes_component/RecipeCard';
+import Recipes from './components/Recipes_component/Recipes';
 import Register from './components/Registro';
 
 // Crear el contexto de autenticación
@@ -19,6 +21,8 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/pagina_principal" element={<PaginaPrincipal />} />
             <Route path="/registro" element={<Register />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/recipes/:recipeId" element={<RecipeCard />} />
           </Routes>
         </div>
       </BrowserRouter>
